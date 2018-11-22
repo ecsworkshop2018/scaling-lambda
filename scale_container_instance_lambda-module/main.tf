196138476306e68ea42034ae7abbbc789ff06418
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-11-22 12:50:29 +0530

    Updated role reference.

--- scale_container_instance_lambda-module/main.tf
+++ scale_container_instance_lambda-module/main.tf
@@ -2,7 +2,7 @@
 resource "aws_lambda_function" "scale_ecs_cluster_container_instances_lambda" {
   filename         = "scaling_lambda.zip"
   function_name    = "scale_ecs_cluster_container_instances_lambda_${var.env}"
-  role             = "${aws_iam_role.iam_role_for_draining_container_instance_lambda.arn}"
+  role             = "${aws_iam_role.iam_role_for_scaling_container_instance_lambda.arn}"
   handler          = "publish_schedulable_container_metric.lambda_handler"
   source_code_hash = "${var.source_code_hash}"
   runtime          = "python3.6"
